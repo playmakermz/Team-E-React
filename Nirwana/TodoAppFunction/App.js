c82e642f46917539c4140cf341453cd7c2f574e0
nirwana_pr <nirwana.pratiwi@ui.ac.id>
2024-06-14 09:01:07 +0700

    tugas todo app function component

---
+++ Nirwana/TodoAppFunction/App.js
@@ -0,0 +1,81 @@
+import React, {useState} from 'react';
+import Todos from './Todos';
+import TodoForm from './TodoForm';
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Finish Progate React Course",
+      completed: false
+    },
+    {
+      id: 2,
+      title: "Have lunch with Guru Domba",
+      completed: false
+    },
+    {
+      id: 3,
+      title: "Study React with Ninja Ken",
+      completed: false
+    }
+  ])
+
+  //console.log(todos);
+
+  const toggleCompleted = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed;
+      }
+      //kembaliin todo yang completed === true
+      return todo;
+    })
+    //di-set deh sama todo yg datanya udah diperbarui
+    setTodos(updatedTodos);
+  }
+
+  const deleteTodo = (todoId) => {
+    setTodos(todos.filter((todo) => todo.id !== todoId));
+  }
+
+  const addTodo = (todoTitle) => {
+    if (todoTitle === '') {
+      return;
+    }
+
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoTitle,
+      completed: false,
+    }
+
+    const updatedTodos = todos.concat(newTodo);
+    setTodos(updatedTodos);
+
+  }
+
+  return (
+    <div style={listStyle.container}>
+      <h1 style={listStyle.title}>My Todo List</h1>
+      <TodoForm addTodo={addTodo}/>
+      <Todos 
+      todos={todos} 
+      toggleCompleted={toggleCompleted} 
+      deleteTodo={deleteTodo}
+      />
+    </div>
+  );
+}
+
+const listStyle = {
+  container: {
+    textAlign: 'center',
+    padding: '40px',
+  },
+  title: {
+    fontSize: '36px'
+  }
+}
+
+export default App;
