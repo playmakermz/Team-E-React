6ff8dc296100b0f5ef071ef6176866251a903d95
Muhammad Yusuf <yusuftkr077@gmail.com>
2024-06-22 14:32:18 +0700

    Tugas weather app yusuf

---
+++ Yusuf/TUGAS WEATHER APP MUHAMMAD YUSUF/app/(tabs)/index.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { View, StyleSheet, ActivityIndicator, Text } from 'react-native'
+import { BASE_URL, API_KEY } from './src/constant'
+import WeatherSearch from './src/components/weatherSearch'
+import WeatherInfo from './src/components/weatherInfo'
+
+const App = () => {
+  const [weatherData, setWeatherData] = useState(null)
+  const [status, setStatus] = useState('')
+
+  const searchWeather = (location) => {
+    setStatus('loading')
+    axios
+      .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
+      .then((response) => {
+        const data = response.data
+        data.visibility /= 1000
+        data.visibility = data.visibility.toFixed(2)
+        data.main.temp -= 273.15
+        data.main.temp = data.main.temp.toFixed(2)
+        setWeatherData(data)
+        setStatus('success')
+      })
+      .catch((error) => {
+        console.log(error)
+        setStatus('error')
+      })
+  }
+
+  const renderComponent = () => {
+    switch (status) {
+      case 'loading':
+        return <ActivityIndicator size="large" />
+      case 'success':
+        return <WeatherInfo weatherData={weatherData} />
+      case 'error':
+        return <Text>Something went wrong. Please try again with a correct city name.</Text>
+      default:
+        return null
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <WeatherSearch searchWeather={searchWeather} />
+      <View style={styles.marginTop20}>{renderComponent()}</View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  marginTop20: {
+    marginTop: 20,
+  },
+})
+
+export default App
---
+++ Yusuf/TUGAS WEATHER APP MUHAMMAD YUSUF/app/(tabs)/src/constant.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export const API_KEY = '1f17d707d32483ac8eb78275314adf12'; //API Key yang telah didapatkan 
